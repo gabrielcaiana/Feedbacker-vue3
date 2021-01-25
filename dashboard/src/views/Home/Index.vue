d24ba322f27a4d748073c5712b022ec9abed5bf8
Gabriel <gabrielcaiana@hotmail.com>
2021-01-25 13:00:37 -0300

    criando componente de contato

--- dashboard/src/views/Home/Index.vue
+++ dashboard/src/views/Home/Index.vue
@@ -1,7 +1,43 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a Home</h1>
+  <custom-header
+    @create-account="handlerAccountCreate"
+    @login="handlerLogin"
+   />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">Feedbacker © 2021</p>
+  </div>
 </template>
 
 <script>
-export default {}
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader'
+import Contact from './Contact'
+export default {
+  components: { CustomHeader, Contact },
+
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handlerLogin () {
+    }
+
+    function handlerAccountCreate () {
+    }
+
+    return {
+      handlerLogin,
+      handlerAccountCreate
+    }
+  }
+}
 </script>
