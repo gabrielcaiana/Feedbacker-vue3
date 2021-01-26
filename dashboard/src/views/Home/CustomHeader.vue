769c867e10e2b4eb5a0ae660dc3f98bcc6428f44
Gabriel <gabrielcaiana@hotmail.com>
2021-01-26 13:01:29 -0300

    criando conteúdo principal da página inicial

--- dashboard/src/views/Home/CustomHeader.vue
+++ dashboard/src/views/Home/CustomHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
-    <div class="flex flex-col w-4/5 max-w-6xl">
+    <div class="header-group">
       <div class="flex items-center justify-between py-10">
         <div class="w-28 lg:w-36">
           <img
@@ -22,7 +22,25 @@
           </button>
         </div>
       </div>
-      <div></div>
+      <div class="flex flex-col mt-28">
+        <h1 class="text-4xl font-black text-white">
+          Tenha um feedback. <br />
+          e faça seus clientes mais <br class="hidden lg:inline-block" />
+          felizes!
+        </h1>
+        <p class="text-lg font-medium text-white">
+          Receba ideias, reclamações e feedbacks com um
+          <br class="hidden lg:inline-block" />
+          simples widget na página.
+        </p>
+        <div>
+          <button
+            class="px-6 py-2 mt-10 font-bold bg-white rounded-full text-brand-main focus:outline-none"
+          >
+            Crie uma conta grátis
+          </button>
+        </div>
+      </div>
     </div>
   </header>
 </template>
@@ -35,5 +53,18 @@ export default {}
 .header {
   @apply bg-brand-main w-full flex justify-center;
   height: 700px;
+}
+
+.header-group {
+  @apply flex flex-col w-4/5 max-w-6xl;
+}
+
+@media (min-width: 640px) {
+  .header-group {
+    background-image: url(../../assets/images/blue_balloons.png);
+    background-size: 628px;
+    background-position: 90% 100%;
+    background-repeat: no-repeat;
+  }
 }
 </style>
