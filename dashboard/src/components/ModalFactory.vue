761dd0ab628d0a0f4229e7b45125b840eb089859
Gabriel <gabrielcaiana@hotmail.com>
2021-01-26 13:57:48 -0300

    criando um modal factory

---
+++ dashboard/src/components/ModalFactory.vue
@@ -0,0 +1,43 @@
+<template>
+  <teleoport to="body">
+    <div 
+    v-if="state.isActive"
+    class=" fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50 "
+    @click="handlerModalToogle({state: false})"
+    >
+     <div class="fixed mx-10">
+      <div class="flex flex-col overflow-hideen bg-white rounded-lg animate__animated animate__fadeInUp animate__faster">
+       <div>
+        <div class="flex flex-col px-12 py-10 bg-white">
+         <component :is="state.component" />
+        </div>
+       </div>
+      </div>
+     </div>
+    </div>
+  </teleoport>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+const DEFAULT_WIDTH = 'W-3/4 lg:w-1/3'
+export default {
+ setup () {
+  const state = reactive({
+   isActive: false,
+   component: {},
+   props: {},
+   width: DEFAULT_WIDTH
+  })
+
+  function handlerModalToogle({status}) {
+   
+  }
+
+  return {
+   state
+  }
+ }
+};
+</script>
